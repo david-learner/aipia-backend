7f23bf8e7cec851a6129d71005048711e0ebb0b8
Taewon Hwang <taewon@paycrux.co.kr>
2025-10-04 19:23:15 +0900

    프로젝트 기초 파일 추가

---
+++ src/main/java/com/aipiabackend/AipiaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.aipiabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AipiaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AipiaBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aipiabackend/AipiaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aipiabackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AipiaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
