58e82de07394081b9a64461b57f55245258f3312
Taewon Hwang <taewon@paycrux.co.kr>
2025-10-08 15:01:57 +0900

    공통 응답 모델 수정

--- src/main/java/com/aipiabackend/support/dto/ErrorResponse.java
+++ src/main/java/com/aipiabackend/support/dto/ErrorResponse.java
@@ -1,4 +1,7 @@
 package com.aipiabackend.support.dto;
 
-public record ErrorResponse(String code, String message) {
+public record ErrorResponse(
+    String code,
+    String message,
+    String detail) {
 }
