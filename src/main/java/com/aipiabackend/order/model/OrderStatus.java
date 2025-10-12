c7f8c6b9d2f4d6a48144a4bd1e3e80b1d90cdcc1
Taewon Hwang <taewon@paycrux.co.kr>
2025-10-12 16:57:27 +0900

    주문 상태 이름 변경

--- src/main/java/com/aipiabackend/order/model/OrderStatus.java
+++ src/main/java/com/aipiabackend/order/model/OrderStatus.java
@@ -3,5 +3,5 @@ package com.aipiabackend.order.model;
 public enum OrderStatus {
     PENDING,    // 주문 대기: 주문은 생성되었으나 결제가 완료되지 않은 상태
     CANCELED,   // 주문 취소: 사용자가 주문을 취소한 상태
-    COMPLETED   // 주문 완료: 주문 생성 후 결제가 완료된 상태
+    SUCCEEDED   // 주문 성공: 주문 생성 후 결제가 완료된 상태
 }
